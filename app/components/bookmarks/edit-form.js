ba9f60618ddb43605576b01024555a7dfc87dc40
Dan Leehr <dan.leehr@duke.edu>
2016-11-11 15:02:07 -0500

    bookmarks/new now creates a new empty object, and save hook actually saves it
    - Uses promises then and catch, with the =>. I'm not sure this makes sense
    - We also created a bookmark property on the component but I don't know why or how that works

--- app/components/bookmarks/edit-form.js
+++ app/components/bookmarks/edit-form.js
@@ -1,8 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  bookmark: null,
+  router: Ember.inject.service('-routing'),
   actions: {
-    save(bookmark) { Ember.Logger.info('save called'); },
+    save(bookmark) {
+      Ember.Logger.info('save called');
+      bookmark.save()
+      .then(
+        (value) => {
+          Ember.Logger.info(value);
+          this.get('router').transitionTo('bookmarks');
+        }
+      ).catch(
+        () => {
+          Ember.Logger.info('failure to save');
+        }
+      );
+    },
     cancel() { Ember.Logger.info('cancel called'); }
   }
 });
