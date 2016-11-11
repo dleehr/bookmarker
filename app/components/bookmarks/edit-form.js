95ab791e50e3214c8c58375fa7107f089c81e85e
Dan Leehr <dan.leehr@duke.edu>
2016-11-11 14:51:01 -0500

    adds bookmarks/edit-form component with form and dummy save/cancel methods

---
+++ app/components/bookmarks/edit-form.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    save(bookmark) { Ember.Logger.info('save called'); },
+    cancel() { Ember.Logger.info('cancel called'); }
+  }
+});
