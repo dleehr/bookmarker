cad5d1388c03697f87ab98e315ee7518c309fe33
Dan Leehr <dan.leehr@duke.edu>
2016-11-14 16:18:53 -0500

    Adds acceptance test for bookmarks/delete
    - creates a empty user
    - creates a bookmark, visits it, deletes it, and asserts we get redirected

---
+++ tests/acceptance/bookmark-delete-test.js
@@ -0,0 +1,18 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'bookmarker/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | bookmark delete');
+
+test('visiting /bookmark-delete', function(assert) {
+  let user = server.create('user', {});
+  let bookmark = server.create('bookmark', {
+    title: 'Test Bookmark',
+    userId: user.id
+  });
+  visit(`/bookmarks/edit/${bookmark.id}`);
+  click('button#delete');
+  andThen(function() {
+    assert.equal(currentURL(), '/bookmarks');
+  });
+
+});
